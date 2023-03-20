fe7c20c45fbfa22ae6df8e49103b6bc171827897
Mithelash <mithelash.c@hotmail.com>
2023-03-20 17:59:01 +0000

    Fundamentals React Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
+import Navbar from "./Navbar";
+
 export default function App () {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/players" element={<Players/>} />
+            <Route path='/teams' element={<Teams/>} />
+          </Routes>
+        </div> 
+      </Router>
   )
 }
